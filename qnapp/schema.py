c5961a69fe6deeb879765ee8749509695b830c38
pradeepmaurya-neo <pradeep.maurya@neosoftmail.com>
2022-05-09 13:51:46 +0530

    use resolve method

--- qnapp/schema.py
+++ qnapp/schema.py
@@ -32,9 +32,20 @@ class AnswersType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
 
-    all_quizzes = DjangoListField(QuizzesType)
+    # all_quizzes = DjangoListField(QuizzesType)
+    # all_quizzes = graphene.Field(QuizzesType, id=graphene.Int())
+    all_question = DjangoListField(QuestionType, id = graphene.Int())
+    all_answers = graphene.List(AnswersType, id = graphene.Int())
+
+    def resolve_all_quizzes(root, info, id):
+        return Quizzes.objects.get(pk=id)
 
 
+    def resolve_all_question(root, info, id):
+        return Question.objects.get(pk=id)
+
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.get(pk=id)
 
 
 schema = graphene.Schema(query=Query)
