d961c1a529b24ed74e7f37a738aef84e151f6445
pradeepmaurya-neo <pradeep.maurya@neosoftmail.com>
2022-05-09 18:59:50 +0530

    add filter method

--- qnapp/schema.py
+++ qnapp/schema.py
@@ -34,18 +34,18 @@ class Query(graphene.ObjectType):
 
     # all_quizzes = DjangoListField(QuizzesType)
     # all_quizzes = graphene.Field(QuizzesType, id=graphene.Int())
-    all_question = DjangoListField(QuestionType, id = graphene.Int())
+    all_question = graphene.Field(QuestionType, id = graphene.Int())
     all_answers = graphene.List(AnswersType, id = graphene.Int())
 
-    def resolve_all_quizzes(root, info, id):
-        return Quizzes.objects.get(pk=id)
+    # def resolve_all_quizzes(root, info, id):
+    #     return Quizzes.objects.get(pk=id)
 
 
     def resolve_all_question(root, info, id):
         return Question.objects.get(pk=id)
 
     def resolve_all_answers(root, info, id):
-        return Answer.objects.get(pk=id)
+        return Answer.objects.filter(questions=id)
 
 
 schema = graphene.Schema(query=Query)
